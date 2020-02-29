26ffb331c5b80f51c007da9b3750d1d3683c9396
LjuboM <ljubo.galcon@gmail.com>
2020-02-29 17:21:50 +0100

    First Spring Boot

---
+++ src/test/java/com/example/Betting/BettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Betting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
